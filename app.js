bda93ca9989d2ed5c3685043d524d52b84389c1c
Dzordanis <100812266+Dzordanis@users.noreply.github.com>
2022-09-02 09:00:38 +0300

    coment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require('express-session')
 const router = require('./router/mainRouter')
 app.listen(4000)
 console.log('server started on port 4000')
-mongoose.connect('mongodb+srv://MantasAksenavicius:5nk7TrCYPGetp5gr@cluster0.3lc4zbg.mongodb.net/?retryWrites=true&w=majority') //pakeisti username ir pass
+mongoose.connect('mongodb+srv://MantasAksenavicius:5nk7TrCYPGetp5gr@cluster0.3lc4zbg.mongodb.net/?retryWrites=true&w=majority') 
     .then(res => console.log('DataBase connected'))
     .catch(e => console.log(e));
 app.use(cors({
